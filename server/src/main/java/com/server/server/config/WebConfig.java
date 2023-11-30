f47fa3f44fe38159c23f33d3a9aca655f06ce45b
MariusDevelops <brazasmarius@gmail.com>
2023-11-30 12:38:38 +0200

    refactor: deleted comments

--- server/src/main/java/com/server/server/config/WebConfig.java
+++ server/src/main/java/com/server/server/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.server.server.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,20 +18,3 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 }
-
-//package com.server.server;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/*")
-//                .allowedOrigins("http://localhost:3000/", "http://localhost:8080", "http://localhost:5173")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("");
-//    }
-//}
