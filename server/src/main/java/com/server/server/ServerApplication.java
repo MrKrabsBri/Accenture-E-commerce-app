31db1bec63d2f9d75123d4538db6a4228d137428
MrKrabsBri <julius.bri@outlook.com>
2023-11-30 10:33:09 +0200

    feat: password hashing method

--- server/src/main/java/com/server/server/ServerApplication.java
+++ server/src/main/java/com/server/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.server.server;
 
+import com.server.server.passwordEncoding.PasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ServerApplication.class, args);
     }
 
---
+++ server/src/main/java/com/server/server/passwordEncoding/PasswordEncoder.java
@@ -0,0 +1,56 @@
+package com.server.server.passwordEncoding;
+
+import org.springframework.stereotype.Service;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+public class PasswordEncoder {
+    //Check if password contains at least one special character
+    //check if it contains at least one number
+    //check if password length is 8 or more symbols
+    public final String SPECIAL_CHARACTERS = "!@#$%^&*()-_+=[]{}|\\;:'\",.<>?/";
+    public final String NUMBERS = "0123456789";
+
+    public String hashPassword(String password) {
+
+        MessageDigest messageDigest;
+        boolean containsSpecialCharacter = false;
+        for (char specialChar : SPECIAL_CHARACTERS.toCharArray()) {
+            if (password.contains(String.valueOf(specialChar))) {
+                containsSpecialCharacter = true;
+                break;
+            }
+        }
+        boolean containsNumber = false;
+        for (char number : NUMBERS.toCharArray()) {
+            if (password.contains(String.valueOf(number))) {
+                containsNumber = true;
+                break;
+            }
+        }
+        boolean passwordLength = password.length() >= 8 && password.length() <= 30;
+
+        if (containsSpecialCharacter && containsNumber && passwordLength){
+            try {
+                messageDigest = MessageDigest.getInstance("SHA-256");
+                // Convert the password string to bytes
+                byte[] passwordBytes = password.getBytes();
+                // Update the message digest with the password bytes
+                byte[] hashedBytes = messageDigest.digest(passwordBytes);
+                // Convert the hashed bytes to a hexadecimal representation
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte hashedByte : hashedBytes) {
+                    stringBuilder.append(Integer.toString((hashedByte & 0xff) + 0x100, 16).substring(1));
+                }
+
+                return stringBuilder.toString();
+
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        throw new IllegalArgumentException("Password does not meet the required conditions");
+    }
+}
