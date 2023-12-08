23cd0255380fc818554948a1c26020d84aabfe31
Bebrucha <99428206+Bebrucha@users.noreply.github.com>
2023-12-08 15:50:29 +0200

    feat: empty cart visuals

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -2,11 +2,22 @@ import React, { useEffect, useState } from "react";
 import { getCartItemsByUserId, getItemsByIds } from "../../services/api";
 import Navbar from "../../components/Navbar";
 import CartList from "../../components/CartList";
-import { Typography, Container } from "@mui/material";
+import {
+  Typography,
+  Container,
+  Box,
+  Button,
+  CircularProgress,
+} from "@mui/material";
 import CartFooter from "../../components/CartFooter";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
+import EmptyCart from "../../assets/empty-cart.svg";
+
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
   const [detailedItems, setDetailedItems] = useState([]);
+  const [loading, setLoading] = useState(true);
   const userId = JSON.parse(localStorage.getItem("authenticatedUser"))?.userId;
 
   useEffect(() => {
@@ -22,6 +33,8 @@ const CartPage = () => {
         }
       } catch (error) {
         console.error("Error fetching cart items:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchCartItems();
@@ -39,6 +52,13 @@ const CartPage = () => {
     setCartItems(updatedCartItems);
     setDetailedItems(updatedDetailedItems);
   };
+
+  const navigate = useNavigate();
+
+  const handleAddMoreItems = () => {
+    navigate("/");
+  };
+
   return (
     <div>
       <Navbar />
@@ -46,12 +66,36 @@ const CartPage = () => {
         <Typography variant="h4" align="center" pt={3}>
           Your cart
         </Typography>
-        <CartList
-          detailedItems={detailedItems}
-          cartData={cartItems}
-          removeFromCart={removeFromCart}
-        />
-        <CartFooter detailedItems={detailedItems} />
+        {loading ? (
+          <Box display="flex" justifyContent="center" mt={3}>
+            <CircularProgress />
+          </Box>
+        ) : detailedItems.length === 0 ? (
+          <Box textAlign="center" mt={3}>
+            <img src={EmptyCart} alt="Empty Cart" width={200} />
+            <br />
+            <Typography variant="h5">Cart is empty</Typography>
+            <br />
+            <Button
+              variant="contained"
+              color="success"
+              startIcon={<ShoppingCartIcon />}
+              onClick={handleAddMoreItems}
+              mt={2}
+            >
+              Add items to cart
+            </Button>
+          </Box>
+        ) : (
+          <>
+            <CartList
+              detailedItems={detailedItems}
+              cartData={cartItems}
+              removeFromCart={removeFromCart}
+            />
+            <CartFooter detailedItems={detailedItems} />
+          </>
+        )}
       </Container>
     </div>
   );
