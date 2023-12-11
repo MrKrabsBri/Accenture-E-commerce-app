e8bd4770edaa188d3b519fd19e1261404bcb7aa4
MrKrabsBri <julius.bri@outlook.com>
2023-12-11 03:05:33 +0200

    added -add, display all, display by userId- shopping basket operations

--- server/src/test/java/com/server/server/shoppingCartTest/TestShoppingCartController.java
+++ server/src/test/java/com/server/server/shoppingCartTest/TestShoppingCartController.java
@@ -18,6 +18,12 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
 @WebMvcTest(controllers = ShoppingCartController.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
@@ -32,7 +38,7 @@ public class TestShoppingCartController {
     private ObjectMapper objectMapper;
 
     @Test
-    void creates_a_ShoppingCartItem() throws Exception {
+    void test_adds_an_item_to_shopping_cart() throws Exception {
 
         ShoppingCartItem item = new ShoppingCartItem(2, 13, 1L, 2);
 
@@ -41,6 +47,32 @@ public class TestShoppingCartController {
                 .content(objectMapper.writeValueAsString(item)));
 
         response.andExpect(MockMvcResultMatchers.status().isCreated());
-
     }
+    @Test
+    void test_get_All_ShoppingCartItems() throws Exception {
+        // Mock the behavior of the service to return a list of items
+        List<ShoppingCartItem> itemList = Arrays.asList(
+                new ShoppingCartItem(1L, 13, 1L, 3),
+                new ShoppingCartItem(2L, 14, 2L, 1)
+        );
+        when(shoppingCartService.getCartItems()).thenReturn(itemList);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/cart/items"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(itemList)));
+    }
+
+    @Test
+    void gets_ShoppingCartItem_By_ID() throws Exception {
+        int userId= 14;
+        List<ShoppingCartItem> itemList = Collections.singletonList(new ShoppingCartItem(2L, userId, 1L, 3));
+        when(shoppingCartService.getCartItemsByUserId(userId)).thenReturn(itemList);
+
+        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("/api/cart/items/{userId}",userId));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(itemList)));
+    }
+
 }
